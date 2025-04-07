408a2ac5f0c804dacad6b47b0884d7dea295d9ac
Aman Kumar <trulyaman25@gmail.com>
2025-04-07 18:21:56 +0530

    Added Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './globalStyles.css'
 import RoutesConfig from './routes.jsx'
 
+import { Analytics } from "@vercel/analytics/react"
+
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
 		<RoutesConfig />
+		<Analytics />
 	</StrictMode>,
 )
